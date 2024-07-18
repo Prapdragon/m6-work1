6bcb629e1631d5206d1a3347f5a7bc5b72975d6d
Prapdragon <139678437+Prapdragon@users.noreply.github.com>
2024-07-18 08:24:57 +0700

    Add files via upload

---
+++ งาน 1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=, initial-scale=1.0">
+      <title>Document</title>
+</head>
+<body>
+      <div class="container">
+            <div class="row">
+                  <?php
+                        echo "<h1>PHP & MariaDB</h1>"
+                        ?>
+                  <?php      
+                        echo "<h1 style ='color:Blue'>สิ่งที่ต้องติดตั้ง</h1>"
+                  ?>
+                  <?php
+                        echo"<li>Apache</li>"
+                  ?>
+                  <?php
+                        echo "<li>PHP</li>"
+                  ?>
+                  <?php
+                        echo "<li>MariaDB</li>"
+                        ?>
+            </div>
+      </div>
+
+</body>
+</html>
